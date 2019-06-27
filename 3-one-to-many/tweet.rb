b0e77d2833fe4e1dd74339fb1d539c3296f8deab
github username <github email address>
2019-06-27 11:22:55 +0100

    Added One to Many

---
+++ 3-one-to-many/tweet.rb
@@ -0,0 +1,25 @@
+class Tweet
+  # Get the message
+  # Get the user it belongs to
+  attr_reader(:message, :user)
+
+  # Class variable to keep track of every instance of Tweet
+  @@all = []
+
+  # Get a list of every instance of Tweet that's been created
+  def self.all
+    @@all
+  end
+
+  # Create a new Tweet with a message and the user it belongs to
+  def initialize(message:, user:)
+    @message = message
+    @user = user
+    @@all << self
+  end
+
+  # Get the username of the user it belongs to
+  def username
+    self.user.username # By using self to get the Tweet that called this method, then getting the instance of User that this instance of Tweet belongs to, we can then get that User's username
+  end
+end
---
+++ 3-one-to-many/user.rb
@@ -0,0 +1,20 @@
+class User
+
+  # Get the user's username
+  attr_reader(:username)
+
+  # Create a new user with a username
+  def initialize(username)
+    @username = username
+  end
+
+  # Post a tweet
+  def post_tweet(message)
+    new_tweet = Tweet.new(message: message, user: self) # We pass in ourselves (The instance of User that called this post_tweet method) as the user that this instance of Tweet belongs to
+  end
+
+  def tweets
+    Tweet.all.select { |tweet| tweet.user == self } # Go to our single source of truth to get all the instances of Tweet, iterate through it and only select the tweets that belong to the same instance of User that called this tweets method
+  end
+
+end
