13269bc45bdc465aecf4aa4a787ee90909133710
github username <github email address>
2019-07-15 13:11:25 +0100

    Added comments to Intro to Sinatra and MVC

--- 8-intro-to-sinatra-and-mvc/app/controllers/application_controller.rb
+++ 8-intro-to-sinatra-and-mvc/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
-class ApplicationController < Sinatra::Base
+# The parent controller for the application; all other controllers will inherit from this one.
+class ApplicationController < Sinatra::Base # Inherits from Sinatra base, giving it access to methods that will allow us to define our routes.
 
+  # Tells this controller (and all that inherit from it) to look for the views directory one directory above the one this file is in when rendering views via erb.
   set :views, Proc.new { File.join(root, "../views")}
 end
