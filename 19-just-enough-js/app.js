ee79f2f13c8ce7e43cdfbc9a039b1469ce5b8303
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2019-08-05 15:23:39 +0100

    edits

---
+++ 19-just-enough-js/app.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", event => {
+  const title = document.querySelector("h1");
+  title.innerText = "NOT BOOKS";
+
+  function hello() {
+    console.log("hello world");
+  }
+});
+
+function hi() {
+  console.log("hi");
+}
